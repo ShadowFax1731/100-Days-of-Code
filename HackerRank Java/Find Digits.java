2ca29a6f9458e3ebab72b8c2e2da3d1996f7b644
ShadowFax1731 <khare.rachit98@gmail.com>
2019-12-02 17:20:52 +0530

    Added Find Digits

---
+++ HackerRank Java/Find Digits.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Completed the findDigits function below.
+    static int findDigits(int n) {
+        int m = n, d, ctr = 0;
+        while (n > 0){
+            d = n % 10; 
+            n = n / 10;
+            if (d == 0) 
+                continue;
+            
+            else if (m % d == 0){
+                ctr++;
+            }
+        }
+        return ctr;
+    }
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int t, n;
+        t = sc.nextInt();
+        for (int i = 1; i <= t; i++){
+            int n = sc.nextInt();
+            int result = findDigits(n);
+            System.out.println(result);
+        }
+
+        sc.close();
+    }
